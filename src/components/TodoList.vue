c45f1d8eef36cfd19a271a7d7da909ee95deff17
Manish Kumar <manishkumar12340@gmail.com>
2024-07-29 15:06:29 +0530

    migrated to vue3

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,7 +5,7 @@
       <div class="card-inner">
         <ul class="list-group">
           <li class="list-group-item d-flex p-3" v-for="(todo, index) in todos" :key="index">
-            <div class="flex-fill">{{ todo | capitalize }}</div>
+            <div class="flex-fill">{{ capitalizeTodoTitle(todo) }}</div>
             <div class="list-item-delete">
               <button type="button" @click="removeTodo(index)" class="btn btn-danger" title="Delete">X</button>
             </div>
@@ -16,21 +16,16 @@
   </div>
 </template>
 
-<script>
-import { mapState, mapActions } from 'vuex';
-export default {
-  name: "TodoList",
-  filters: {
-    capitalize(value) {
-      if (!value) return '';
-      return value.charAt(0).toUpperCase() + value.slice(1);
-    }
-  },
-  computed: {
-    ...mapState(['todos']),
-  },
-  methods: {
-    ...mapActions(['removeTodo']),
-  },
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const todos = computed(() => store.state.todos);
+const capitalizeTodoTitle =  (value) => {
+  if (!value) return '';
+  return value.charAt(0).toUpperCase() + value.slice(1);
 };
+
+const removeTodo = (index) => store.dispatch('removeTodo', index);
 </script>
